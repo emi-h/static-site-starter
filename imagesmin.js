d8a96888cbc446038bbc4f0fe04e9c46775dac3a
Emi <76840826+emi-h@users.noreply.github.com>
2022-10-12 18:16:07 +0900

    Update imagemin.js
    output先の指定がよくなかったので、変更

--- imagesmin.js
+++ imagesmin.js
@@ -1,19 +1,17 @@
-const imagemin         = require('imagemin-keep-folder');
-const imageminMozjpeg  = require('imagemin-mozjpeg');
-const imageminPngquant = require('imagemin-pngquant');
-const imageminGifsicle = require('imagemin-gifsicle');
-const imageminSvgo     = require('imagemin-svgo');
+import imagemin from 'imagemin'
+import imageminMozjpeg from 'imagemin-mozjpeg'
+import imageminPngquant from 'imagemin-pngquant'
+import imageminGifsicle from 'imagemin-gifsicle'
+import imageminSvgo from 'imagemin-svgo'
 
 imagemin(['src/assets/images/**/*.{jpg,png,gif,svg}'], {
     plugins: [
         imageminMozjpeg({ quality: 80 }),
-        imageminPngquant({ quality: '65-80' }),
+        imageminPngquant({ quality: [0.6, 0.8]}),
         imageminGifsicle(),
         imageminSvgo()
     ],
-    replaceOutputDir: output => {
-        return output.replace(/images\//, '../../dist/assets/images/')
-    }
+    destination: 'dist/images'
 }).then(() => {
     console.log('Images optimized');
 });
